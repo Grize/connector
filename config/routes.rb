911b2a1d9d55a939f21a4eb13569f83abcce0241
pbarnash <pavel.barnash@bia-tech.ru>
2023-02-22 23:17:07 +0600

    Fix token endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  scope 'api' do
-    scope 'priora' do
-      use_doorkeeper scope: 'v2' do
-        controllers tokens: 'api/priora/v2/tokens'
-        skip_controllers :authorizations, :applications, :authorized_applications
+  namespace 'api' do
+    namespace 'priora' do
+      namespace 'v2' do
+        resources :tokens, only: [:create]
       end
     end
   end
