17a2b1e9c873696752643cbca043f9a37232cfe8
pbarnash <pavel.barnash@bia-tech.ru>
2023-02-22 23:46:59 +0600

    Fix app configs and add secrets

--- app/mixins/app_config.rb
+++ app/mixins/app_config.rb
@@ -4,15 +4,15 @@ module AppConfig
   end
 
   def private_key
-    OpenSSL::PKey::RSA.new(File.read(config['private_certificate_path']))
+    OpenSSL::PKey::RSA.new(Rails.application.credentials.config[:app_private_key])
   end
 
   def app_id
-    config['app_id']
+    Rails.application.credentials.config[:salt_edge][:app_id]
   end
 
   def app_secret
-    config['app_secret']
+    Rails.application.credentials.config[:salt_edge][:app_secret]
   end
 
   private
