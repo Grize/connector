dcba784d4d9c513f115a3cede4e3cbd8087026f3
pbarnash <pavel.barnash@bia-tech.ru>
2023-02-25 00:05:49 +0600

    Fix

--- app/controllers/api/priora/v2/authorizations_controller.rb
+++ app/controllers/api/priora/v2/authorizations_controller.rb
@@ -13,7 +13,7 @@ module Api
 
         def create
           token = Tokens::Create.new(current_user, auth_params).call
-          redirect_to token.redirect_uri, allow_other_host: true
+          redirect_to token.redirect_uri, allow_other_host: true, '_method': :get
         end
 
         private
